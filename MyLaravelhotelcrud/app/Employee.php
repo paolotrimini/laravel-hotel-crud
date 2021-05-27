8369c29be07a01ef33a9adc129a9a3fc4a90bf0a
paolotrimini <paolo.trimini@gmail.com>
2021-05-27 16:08:09 +0200

    creazione migration - model - factory - seeder

--- MyLaravelhotelcrud/app/Employee.php
+++ MyLaravelhotelcrud/app/Employee.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'role',
+        'ral',
+    ];
 }
