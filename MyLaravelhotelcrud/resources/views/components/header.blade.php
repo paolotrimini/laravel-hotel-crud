c224ea31585ae7a0496e402dc40d9e93f68c045c
paolotrimini <paolo.trimini@gmail.com>
2021-05-27 15:59:12 +0200

    caricamento file

---
+++ MyLaravelhotelcrud/resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <h1>HEADER</h1>
+    <a class="btn btn-primary mb-3 mt-1" href="#">
+        CREATE NEW EMPLOYEE
+    </a>
+</header>
---
+++ MyLaravelhotelcrud/resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+    @extends('layouts.main-layout')
+
+    @section('content')
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Employees:</h2>
+
+                </div>
+            </div>
+        </div>
+    @endsection
